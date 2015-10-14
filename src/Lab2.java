ce2926582af9d65299de07dfd6944dff7f26790f
Kris Jordan <krisjordan@gmail.com>
2015-10-14 13:10:32 -0400

    Update boilerplate.

--- src/Lab2.java
+++ src/Lab2.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 /******************************************************************
  * Lab 2
  *
- * Programmer: INSERT YOUR ONYEN HERE!
+ * Programmer: INSERT YOUR NAME HERE!
+ * 
+ * ONYEN: INSERT YOUR ONYEN HERE!
  *
  * Due Date: 10/21/2015 at 11:55pm
  *
@@ -29,37 +31,40 @@ public class Lab2 {
     // The `keyboard` Scanner is provided for you
     Scanner keyboard = new Scanner(System.in);
     
-    println("Welcome!");
+    // Step 3.
+    printLine("Welcome!");
     
-    println("Please enter the *size* of the array you want to create.");
+    // Step 4.
+    printLine("Please enter the *size* of the array you want to create.");
     
-    // TODO: Step 5 - Construct int array ***named multiplesOfThree*** 
-    //                with its size driven by user input.
+    // TODO: Step 5 - Construct multiplesOfThree array with its 
+    //                size decided by user input.
+    int[] multiplesOfThree;
     
     // TODO: Step 6 - Fill array with user inputs
     // You can reuse the prompts in your Step 6 loop below, just uncomment
     // and use where you need it.
-    // println("Please enter an integer that is a multiple of 3.");
-    // println("That is not a multiple of 3. Try again!");
+    // printLine("Please enter an integer that is a multiple of 3.");
+    // printLine("That is not a multiple of 3. Try again!");
     
-    println("What operation would you like to perform on the array?");
-    println("1: Compute the sum");
-    println("2: Compute the average");
-    println("3: Compute both");
-    
-    // Assign the correct values to these variables in Step 8.
-    int sum = 0;
-    double average = 0.0;
+    // Step 7
+    printLine("What operation would you like to perform on the array?");
+    printLine("1: Compute the sum");
+    printLine("2: Compute the average");
+    printLine("3: Compute both");
     
     // TODO: Step 8 - Depending on the user's selection do the needed
     //       calculations and output it as per the instructions.
+    int sum = 0;
+    double average = 0.0;
     
     // Here are some more boilerplate outputs you can uncomment and reuse.
-    // println("You input:");
-    // printIntArray(multiplesOfThree);
-    // println("The sum is " + sum);
-    // println("The average is " + average);
-    // println("Unrecognized Option");
+    printLine("The sum is " + sum);
+    printLine("The average is " + average);
+    printLine("Unrecognized Option");
+    
+    // TODO: Done. Don't forget to submit to Sakai and check
+    //       your submissions!!!
   }
   
   
@@ -104,37 +109,11 @@ public class Lab2 {
    Once that line of code is done running, the program will jump
    _back_ to where it was before and continue along -- kind
    of like inception. 
-  */
-  static void println(String message) {
-    System.out.println(message);
-  }
-  
-  /*   
-   The `printIntArray` method below works similarly, but it takes
-   as its input an array of integers. These inputs are called
-   "arguments". When called with an array whose elements are 3, 6, 9
-   this method will print an output like:
-   
-   { 3, 6, 9 }
-   
-   Notice that the implementation of `printIntArray` contains a loop
-   and a few print statements. By "abstracting" this behavior in
-   a method, we can print out any integer array we want without having
-   to manually write out the code below each time. It's a little
-   program we can reuse within our program!
    
    We'll talk about writing our own methods soon. They're very powerful
-   and a whole lot of fun!
+   and awesome and da real mvp.
   */
-  static void printIntArray(int[] input) {
-    System.out.print("{ ");
-    for(int i = 0; i < input.length; i++) {
-      if(i > 0) {
-        System.out.print(", ");
-      }
-      System.out.print(input[i]);
-    }
-    System.out.print(" }\n");
+  static void printLine(String message) {
+    System.out.println(message);
   }
-
 }
